d9264a851f8a4dc9d1ac53275a8e2c754daaec28
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-10 19:17:20 -0300

    Operaç~eos em um JSON

--- aula4/leitura.js
+++ aula4/leitura.js
@@ -1,3 +1,14 @@
+// String em arquivo para objeto
 const dados = require('./cliente.json');
 console.log(dados);
 console.log(typeof dados);
+
+// Objeto para String
+const clienteEmString = JSON.stringify(dados);
+console.log(clienteEmString);
+console.log(typeof clienteEmString);
+
+// String em variável para objeto
+const cliente = JSON.parse(clienteEmString);
+console.log(cliente);
+console.log(typeof cliente);
