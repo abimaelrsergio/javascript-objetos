ec7bdd220daa50790350643c3dfd4231fcefd15f
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-10 12:58:22 -0300

    Lista de objetos

---
+++ aula2/listaObjetos.js
@@ -0,0 +1,32 @@
+const cliente = {
+    nome: 'João',
+    idade: 24,
+    email: 'joao@gmail.com',
+    telefone: ['1155654566', '119080987']
+}
+console.log(cliente);
+
+cliente.enderecos = [
+    {
+        rua: 'R. Joseph Smith',
+        numero: 135,
+        apartamento: true,
+        complemento: 'ap 934'
+    }
+]
+cliente.enderecos.push({
+    rua: 'R. Manoel Sé',
+    numero: 5,
+    apartamento: false
+})
+cliente.enderecos.push({
+    rua: 'R. Idalino Manoel',
+    numero: 20,
+    apartamento: true,
+    complemento: 'apt 44'
+})
+console.log(cliente.enderecos);
+
+const listaApenasApartamentos = cliente.enderecos.filter((endereco) => endereco.apartamento);
+
+console.log(listaApenasApartamentos);
