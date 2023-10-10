4019a0a85e5a5cc5a5de78764c2399304372f0a0
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-10 19:59:43 -0300

    Ordenando objetos: crescente ou decrescente!

--- aula5/ordenar.js
+++ aula5/ordenar.js
@@ -1,17 +1,20 @@
 const clientes = require('./clientes.json');
 
-function ordenar(lista, propriedade) {
+function ordenar(lista, propriedade, ordem = 'crescente') {
     const resultado = lista.sort((a, b) => {
-        if (a[propriedade] < b[propriedade]){
+        if (a[propriedade] < b[propriedade]) {
             return -1;
-        } else if (a[propriedade] > b[propriedade]){
-            return 1;
-        } else {
-            return 0;
         }
+        if (a[propriedade] > b[propriedade]) {
+            return 1;
+        }
+        return 0;
     })
+    if (ordem !== 'crescente') {
+        resultado.reverse();
+    }
     return resultado;
 }
 
-const ordenadoPorNome = ordenar(clientes, 'nome');
+const ordenadoPorNome = ordenar(clientes, 'nome', 'decrescente');
 console.log(ordenadoPorNome);
