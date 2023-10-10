ba402050b67527b8c0ce27dcf3264e08d44eef93
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-10 12:39:29 -0300

    Tipos de dados e valores

---
+++ aula2/tipos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: 'João',
+    idade: 24,
+    email: 'joao@gmail.com',
+    telefone: ['1155654566', '119080987']
+}
+console.log(cliente);
+console.log(cliente.telefone);
+console.log(cliente.telefone[0]);
+console.log(cliente.telefone[1]);
